b01a388159dee15185fb52cfefa6b5973cdbfce1
milos <milosjovanovicvr88@gmail.com>
2022-03-28 18:35:19 +0900

    add more file

---
+++ resources/js/layouts/basic.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- <div class="App min-w-screen min-h-screen flex flex-col bg-grey-lightest font-sans"> -->
+    <child />
+  <!-- </div> -->
+</template>
+
+<script>
+export default {
+  name: 'BasicLayout'
+}
+</script>
